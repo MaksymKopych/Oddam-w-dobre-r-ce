2782d924ef0c007372eb98043849f3b4f2e5bb39
MaksymKopych <maximkopych@gmail.com>
2022-05-17 14:38:12 +0200

    validacja

--- src/components/HomeContacts.jsx
+++ src/components/HomeContacts.jsx
@@ -3,69 +3,86 @@ import React, { useState } from "react";
 import { db } from "../App";
 import decor from "../assets/Decoration.svg";
 import { HomeFooter } from "./HomeFooter";
-
+import { useForm } from "react-hook-form";
 export const HomeContacts = () => {
-  const [contactForm, setContactForm] = useState({
-    name: "",
-    email: "",
-    massege: "",
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
   });
-  const postContactForm = async (e) => {
-    e.preventDefault();
-    if (contactForm.name && contactForm.email && contactForm.massege) {
-      await addDoc(collection(db, "contacts"), contactForm).then(
-        setContactForm({
-          name: "",
-          email: "",
-          massege: "",
-        })
-      );
-    } else {
-      console.log("Wypelni");
-    }
+  const postContactForm = async (data) => {
+    await addDoc(collection(db, "contacts"), {
+      name: data.name,
+      email: data.contactEmail,
+      message: data.contactMessage,
+    }).then(reset());
+  };
+  const onSubmit = (data) => {
+    postContactForm(data);
   };
   return (
     <div className="home-contacts" id="contacts">
-      <form id="contactForm" onSubmit={postContactForm}>
+      <form id="contactForm" onSubmit={handleSubmit(onSubmit)}>
         <h2>Skontaktuj się z nami</h2>
         <img src={decor} alt="decor" />
         <div className="home-contacts-inputs">
           <div className="home-contacts-input">
             <label htmlFor="name">Wpisz swoje imię</label>
             <input
-              value={contactForm.name}
-              type="text"
-              name="name"
               placeholder="Krzysztof"
-              onChange={(e) => {
-                setContactForm({ ...contactForm, name: e.target.value });
-              }}
+              {...register("name", {
+                required: "Nie może być puste",
+                minLength: {
+                  value: 3,
+                  message: "Imie za krotkie min 3 znaki",
+                },
+              })}
             />
+            {errors?.name && (
+              <p
+                style={{
+                  color: "red",
+                  paddingTop: "10px",
+                }}
+              >
+                {errors?.name?.message || "error"}
+              </p>
+            )}
           </div>
           <div className="home-contacts-input">
-            <label htmlFor="email">Wpisz swój email</label>
+            <label htmlFor="contactEmail">Wpisz swój email</label>
             <input
-              value={contactForm.email}
               type="email"
-              name="email"
               placeholder="abc@xyz.pl"
-              onChange={(e) => {
-                setContactForm({ ...contactForm, email: e.target.value });
-              }}
+              {...register("contactEmail", {
+                required: "Nie może być puste",
+                pattern: {
+                  value: /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/,
+                  message: "Nie prawidlowy Email",
+                },
+              })}
             />
+            {errors?.contactEmail && (
+              <p
+                style={{
+                  color: "red",
+                  paddingTop: "10px",
+                }}
+              >
+                {errors?.contactEmail?.message || "error"}
+              </p>
+            )}
           </div>
         </div>
         <div className="home-contacts-massage">
           <label htmlFor="massage">Wpisz swoją wiadomość</label>
           <textarea
-            value={contactForm.massege}
-            form="contactForm"
-            name="massage"
+            {...register("contactMessage")}
             placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
           asperiores temporibus modi autem voluptatum, eaque maiores! "
-            onChange={(e) => {
-              setContactForm({ ...contactForm, massege: e.target.value });
-            }}
           />
         </div>
         <button>Wyślij</button>
